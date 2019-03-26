8c49e88af7fde625cc402af4891b14caec36668e
elkin5 <syselkin5@gmail.com>
2019-03-25 20:29:09 -0500

    manejo del servidor
    - Reiniciar servidor automaticamnte a los cambios (nodemon)
    - Ver peticiones en la shell (morgan)
    - Uso de express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('listen on port 3000');
+//Setting : configuraciones del servidor
+app.set('port', process.env.PORT || 3000);
+
+//Midlewares : ejecuciones de metodos 
+
+//verificar mensajes en consola de las peticiones 'dev' es la forma como se ven
+app.use(morgan('dev'));
+//ver datos enviados en formato .json
+app.use(express.json());
+
+//Routes : rutas del servidor
+
+// start : inicio del servidor
+app.listen(app.get('port'), () => {
+  console.log(`listen on port ${app.get('port')}`);
 });
